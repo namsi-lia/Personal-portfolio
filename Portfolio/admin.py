921b524ce085615e758e78d0e4ccb51f5be8c5c4
namsi-lia <namsilydia7@gmail.com>
2023-04-05 17:44:45 +0300

    add url field in models and add more configuration on settings.py

--- Portfolio/admin.py
+++ Portfolio/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import portfolioDetails
 
+
 # Register your models here.
 admin.site.register(portfolioDetails)
+
--- Portfolio/models.py
+++ Portfolio/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from any_urlfield.models import AnyUrlField
+
 
 # Create your models here.
 class portfolioDetails(models.Model):
@@ -8,8 +10,9 @@ class portfolioDetails(models.Model):
     summary =models.CharField(max_length=255)
 
     #link to project
-    links = models.URLField(max_length=200)
-
+    #links = models.URLField(max_length=200)
+    url = AnyUrlField("URL", null=True)
+  
 
    ## def __str__(self):
      #   return self.summary
